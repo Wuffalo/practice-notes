ecd73dc28bebbad37bc803db6d7cd389750caaa8
Wuffalo <waminskey@gmail.com>
2019-11-25 23:08:31 -0600

    Created framework for 24 Hour metric tracker. Includes new notes on share drive access.

--- query-df.py
+++ query-df.py
@@ -15,24 +15,29 @@ def format_sheet():
     worksheet.set_column('B:B',45)
     worksheet.set_column('C:C',5)
     worksheet.set_column('D:D',7)
-    worksheet.set_column('E:E',22)
-    worksheet.set_column('F:F',18)
-    worksheet.set_column('G:G',10)
-    worksheet.set_column('H:H',4)
-    worksheet.set_column('I:I',27)
-    worksheet.set_column('J:J',13,format5)
-    worksheet.conditional_format('J2:J100', {'type': 'duplicate',
-                                        'format': format3})
+    worksheet.set_column('E:E',20)
+    worksheet.set_column('F:F',14)
+    worksheet.set_column('G:G',21)
+    worksheet.set_column('H:H',11)
+    worksheet.set_column('I:I',5)
+    worksheet.set_column('J:J',28)
+    worksheet.set_column('K:K',14,format5)
+    worksheet.conditional_format('K2:K4000', {'type': 'duplicate',
+                                              'format': format3})
 
-if os.path.exists("/mnt/c/Users/WMINSKEY/.pen/24Hour.xlsx"):
-  os.remove("/mnt/c/Users/WMINSKEY/.pen/24Hour.xlsx")
+path_to_excel = "/mnt/shared-drive/05 - Office/OTS/Wolf/24Hour.xlsx"
+
+if os.path.exists(path_to_excel):
+  os.remove(path_to_excel)
 
 list_of_files = glob.glob('/mnt/c/Users/WMINSKEY/Downloads/Shipment Order Summary -*.csv') # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
 path_to_SOS = latest_file
+# path_to_excel = "/mnt/c/Users/WMINSKEY/.pen/24Hour.xlsx"
+# path_to_excel = "/mnt/shared-drive/05 - Office/OTS/Wolf/24Hour.xlsx"
 
 # df = pd.read_csv(path_to_excel, parse_dates=[7,8], infer_datetime_format=True)
-df = pd.read_csv(path_to_SOS)
+df = pd.read_csv(path_to_SOS, parse_dates=[11,19], infer_datetime_format=True)
 
 #columns to delete - INITIAL PASS
 df = df.drop(columns=['ORDERKEY','SO','SS','STORERKEY','INCOTERMS','ORDERDATE','ACTUALSHIPDATE','DAYSPASTDUE',
@@ -61,23 +66,36 @@ format3 = workbook.add_format({'bg_color':   '#C6EFCE',
 
 format5 = workbook.add_format({'num_format': '#'})
 
+#OG queries
+# ingram = df['Customer'] == 'Ingram Micro'
+# ingrammx = df['Customer'] == 'Interamerica Forwarding C/O Ingram Micro Mexi'
+# # roanoke = [df['CUSTID'] = 7128 | 
+# roanoke = df[(df['CUSTID'] == '7128') & (df['State'] == 'TX')]
+# OG = df[ingram|ingrammx|roanoke].index
+
 #CREATE QUERIES TO REMOVE
 remove_rtv = df['TYPEDESCR'] == 'RTV Move'
-remove_status = df['Status'] == 'Not Started'
+remove_NS = df['Status'] == 'Not Started'
+remove_Lo = df['Status'] == 'Loaded'
 
-df.drop(df[remove_rtv].index, inplace=True)
+df.drop(df[remove_rtv|remove_NS|remove_Lo].index, inplace=True)
 
-df.drop(df[df.Status == "Not Started" | df.Status == "Shipped Complete"].index, inplace=True)
+# df.drop(df[df.Status == "Not Started"].index, inplace=True)
 
-df.sort_values(by=['Add Date','Carrier','Load ID'], inplace=True)
+#
+# df['Add Hour'] = df['Add Date'].dt.round('H').dt.hour
+df['Add Hour'] = df['Add Date'].dt.floor('1H')
 
-df = df.drop(columns=['TYPEDESCR','CUSTID','PROMISEDATE'])
+df.sort_values(by=['Add Hour','Status','Carrier'], inplace=True)
+# df.sort_values(by=['Add Date','Carrier','Load ID'], inplace=True)
+
+df = df.drop(columns=['TYPEDESCR','CUSTID','PROMISEDATE','Add Hour'])
 
 df.to_excel(writer, sheet_name='24Hour', index=False)
 worksheet = writer.sheets['24Hour']
 format_sheet()
 
-
+writer.save()
 
 # print(df.shape)     #   (rows, columns)
 # print(df.ndim)      #   number of dimensions
